5a12be18877ba5307079c63f215019cb5953ff88
IfWinGold <74502112+IfWinGold@users.noreply.github.com>
2023-01-02 18:39:48 +0900

    NoiseText 추가
    NoiseText 추가

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,13 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using JungExtension.UI;
 
+public enum TestState
+{
+    scrollSnap,
+    toggleSwitch
+}
 public class Test : MonoBehaviour
 {
+    public TestState state;
+    [SerializeField] private ExtensionScrollSnap scrollsnap;
     [SerializeField] private ExtensionToggleSwitch toggleSwitch;
 
     void Start()
     {
-        toggleSwitch.Initialized(true);
+        switch (state)
+        {
+            case TestState.scrollSnap:
+                {
+                    if (scrollsnap.gameObject.activeSelf == false)
+                        scrollsnap.gameObject.SetActive(true);
+                    scrollsnap.Initialized(5);
+                }
+                break;
+            case TestState.toggleSwitch:
+                {
+                    if (toggleSwitch.gameObject.activeSelf == false)
+                        toggleSwitch.gameObject.SetActive(true);
+                    toggleSwitch.Initialized(true);
+                }
+                break;
+        }                
     }
 
     // Update is called once per frame
